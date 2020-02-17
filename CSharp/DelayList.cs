3abb34ff777a0b3e01400796f95afc7eb5a0e654
wraikny <writtenknown@gmail.com>
2020-02-17 17:58:49 +0900

    Add Constructor overload

--- CSharp/DelayList.cs
+++ CSharp/DelayList.cs
@@ -15,10 +15,23 @@ namespace Altseed
             Remove,
         }
 
-        private readonly List<T> objects = new List<T>();
+        private readonly List<T> objects;
         private readonly Dictionary<T, Waiting> waitings = new Dictionary<T, Waiting>();
 
-        public DelayList() { }
+        public DelayList()
+        {
+            objects = new List<T>();
+        }
+
+        public DelayList(int capacity)
+        {
+            objects = new List<T>(capacity);
+        }
+
+        public DelayList(IEnumerable<T> collection)
+        {
+            objects = new List<T>(collection);
+        }
 
         public void Add(T item) => waitings[item] = Waiting.Add;
         public void Remove(T item) => waitings[item] = Waiting.Remove;
